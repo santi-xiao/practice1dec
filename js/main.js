087abe5614702b4094e6cd69b8a8df25a279c49c
Santi Xiao <santixiaorl@gmail.com>
2021-10-19 12:58:38 +0200

    Estilos y funcionalidad apagado y encendido al tablero

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ const createBoard = (rows, columns) => {
   for (let i = 0; i < rows; i++) {
     board.push(new Array());
     for (let x = 0; x < columns; x++) {
-      board[i].push(0);
+      board[i].push(off);
     }
   }
   return board;
@@ -22,9 +22,21 @@ const drawBoard = () => {
     for (let x = 0; x < board[i].length; x++) {
       let square = document.createElement("div");
       square.id = `square${i}-${x}`;
+      square.data = off;
+      square.className = "ligthOff";
+      square.onclick = switchOnOff;
       rowDiv.appendChild(square);
     }
   }
+};
+
+const switchOnOff = (e) => {
+  console.log(e);
+  e.target.data == off ? (e.target.data = on) : (e.target.data = off);
+  console.log(e.target.data);
+  e.target.data == on
+    ? (e.target.className = "ligthOn")
+    : (e.target.className = "ligthOff");
 };
 
 createBoard(6, 6);
